7f3894239a56f4cf6fa4bce109966f1a362ad8fb
InYoung Park <dlsdudg15@naver.com>
2022-09-20 15:31:32 +0900

    Update api.py

--- app/v1/api.py
+++ app/v1/api.py
@@ -1,8 +1,8 @@
 import io
 import cv2
 import numpy as np
-from fastapi import APIRouter, File, UploadFile
 from PIL import Image
+from fastapi import APIRouter, File, UploadFile, Form
 from starlette.responses import StreamingResponse
 
 from app.utils.anime import animefy
@@ -12,26 +12,22 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-def index():
-    return {"message": "Hello World"}
-
-
 @router.post("/profile")
 def post_profile(file: UploadFile = File(...)):
     image = np.array(Image.open(file.file))
-    _, image = cv2.imencode(".png", image)  # pylint: disable=no-member
+    _, image = cv2.imencode(".png", image)
+
     return StreamingResponse(io.BytesIO(image.tobytes()), media_type="image/png")
-
-
-@router.post("/category")
-def post_category():
-    return {"category": "cat", "caption": "A cat"}
 
 
 @router.post("/animefy")
-def post_animefy(file: UploadFile = File(...)):
+def post_animefy(file: UploadFile = File(...), text: str = Form(...)):
     image = np.array(Image.open(file.file))
     image = animefy(image)
-    _, image = cv2.imencode(".png", image)  # pylint: disable=no-member
-    return StreamingResponse(io.BytesIO(image.tobytes()), media_type="image/png")
+    _, image = cv2.imencode(".png", image)
+
+    return StreamingResponse(
+        io.BytesIO(image.tobytes()),
+        media_type="image/png",
+        headers={"x-mtvs-category": "category", "x-mtvs-caption": "caption"},
+    )
