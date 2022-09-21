ac210a3ba1b995ef731b92e8d27dff97f26c395f
InYoung Park <dlsdudg15@naver.com>
2022-09-21 17:16:56 +0900

    Update api.py

--- app/v1/api.py
+++ app/v1/api.py
@@ -1,11 +1,13 @@
 import io
 import cv2
+import json
 import numpy as np
 from PIL import Image
 from fastapi import APIRouter, File, UploadFile, Form
 from starlette.responses import StreamingResponse
 
 from app.utils.anime import animefy
+from app.utils.profile import project_images
 
 router = APIRouter(
     prefix="/v1/api",
@@ -15,6 +17,7 @@ router = APIRouter(
 @router.post("/profile")
 def post_profile(file: UploadFile = File(...)):
     image = np.array(Image.open(file.file))
+    image = project_images(image)
     _, image = cv2.imencode(".png", image)
 
     return StreamingResponse(io.BytesIO(image.tobytes()), media_type="image/png")
@@ -23,11 +26,16 @@ def post_profile(file: UploadFile = File(...)):
 @router.post("/animefy")
 def post_animefy(file: UploadFile = File(...), text: str = Form(...)):
     image = np.array(Image.open(file.file))
+    # print(text)
+    # category, caption = jwh_model(image, text)
+    category = ["category"]
+    caption = "caption"
+    category = json.dumps(category)
     image = animefy(image)
     _, image = cv2.imencode(".png", image)
 
     return StreamingResponse(
         io.BytesIO(image.tobytes()),
         media_type="image/png",
-        headers={"x-mtvs-category": "category", "x-mtvs-caption": "caption"},
+        headers={"x-mtvs-category": category, "x-mtvs-caption": caption},
     )
