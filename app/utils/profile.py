b705fe8973eebef95ce600857c14bad99e158c9b
InYoung Park <dlsdudg15@naver.com>
2022-09-21 19:30:39 +0900

    Update profile.py

--- app/utils/profile.py
+++ app/utils/profile.py
@@ -1,15 +1,36 @@
 import torch
+import numpy as np
+from PIL import Image
 
 import app.stylegan2 as stylegan2
+from app.stylegan2 import utils
+from app.ffhq_dataset.face_alignment import image_align
+from app.ffhq_dataset.landmarks_detector import LandmarksDetector
 
 pixel_min = 1.0
 pixel_max = 1.0
+output_size = 512
+landmarks_model_path = "app/models/shape_predictor_68_face_landmarks.dat"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+landmarks_detector = LandmarksDetector(landmarks_model_path)
+G_PRE = stylegan2.models.load("app/models/G_pretrained.pth").to(device)
 G = stylegan2.models.load("app/models/G_blend.pth")
+G = G.G_synthesis
 G = G.to(device)
 
 
-def project_images(image):
+def project_images(file):
+    image = Image.open(file)
+    img_name = "temp." + image.format
+    image.save("app/images/raw/" + img_name)
+    raw_img_path = "app/images/raw/" + img_name
+    aligned_img_path = "app/images/aligned/temp.png"
+
+    for face_landmarks in landmarks_detector.get_landmarks(raw_img_path):
+        image_align(raw_img_path, aligned_img_path, face_landmarks, output_size)
+
+    image = Image.open(aligned_img_path)
+    image = np.array(image)
     image = torch.from_numpy(image).to(device)
 
     lpips_model = stylegan2.external_models.lpips.LPIPS_VGG16(
@@ -17,7 +38,7 @@ def project_images(image):
     )
 
     proj = stylegan2.project.Projector(
-        G=G,
+        G=G_PRE,
         dlatent_device=device,
         lpips_model=lpips_model,
         dlatent_avg_samples=2500,
@@ -30,4 +51,11 @@ def project_images(image):
         verbose=True,
     )
 
-    return image.cpu().numpy()
+    latents = proj.get_dlatent().cpu().detach().numpy()
+    np.save("app/images/latent/temp.npy", latents[0])
+    latent = np.load("app/images/latent/temp.npy", allow_pickle=True)
+    latent = torch.tensor(latent[np.newaxis, ...]).to(device)
+    out = G(latent)
+    out = utils.tensor_to_PIL(out, pixel_min=-1, pixel_max=1)[0]
+
+    return np.array(out)
