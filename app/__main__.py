b2c8e2c351efe7b543a4ff24689b61366c065bb2
InYoung Park <dlsdudg15@naver.com>
2022-09-23 09:20:27 +0900

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -3,6 +3,4 @@ import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
-    from app.utils.category_classes import *
-
     uvicorn.run(app, host="0.0.0.0", port=8000, log_config="log.ini")
