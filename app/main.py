db132ffc0b334eaea5da86577e9ae62265cc2f2f
InYoung Park <dlsdudg15@naver.com>
2022-09-28 09:15:30 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
@@ -26,3 +27,28 @@ def get_application():
 
 
 app = get_application()
+
+
+def custom_openapi():
+    # cache the generated schema
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    # custom settings
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="2.5.0",
+        description="This is a very custom OpenAPI schema",
+        routes=app.routes,
+    )
+
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+
+    app.openapi_schema = openapi_schema
+
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
